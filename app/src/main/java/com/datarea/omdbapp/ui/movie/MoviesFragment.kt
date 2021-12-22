f29be0911b18f22d1f5a8e0435dfbd5dd819783f
DILAN ATA <ata.dilan1@gmail.com>
2021-12-22 15:07:20 +0300

    added adapter for movie list

--- app/src/main/java/com/datarea/omdbapp/ui/movie/MoviesFragment.kt
+++ app/src/main/java/com/datarea/omdbapp/ui/movie/MoviesFragment.kt
@@ -5,16 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.navGraphViewModels
 import com.datarea.omdbapp.R
+import com.datarea.omdbapp.api.model.Movie
+import com.datarea.omdbapp.base.BaseFragment
+import com.datarea.omdbapp.databinding.FragmentMoviesBinding
+import com.datarea.omdbapp.ui.movie.adapter.MovieAdapter
+import com.datarea.omdbapp.ui.movie.adapter.OnClickListener
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 
-class MoviesFragment : Fragment() {
+@Suppress("COMPATIBILITY_WARNING")
+@ExperimentalCoroutinesApi
+@FlowPreview
+@AndroidEntryPoint
+class MoviesFragment : BaseFragment<FragmentMoviesBinding>(R.layout.fragment_movies), OnClickListener {
+
+    private val movieVM: MovieVM by navGraphViewModels(R.id.nav_graph) {
+        defaultViewModelProviderFactory
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movies, container, false)
+    }
+
+    override fun onClickForDetail(movie: Movie) {
+        TODO("Not yet implemented")
     }
 }
